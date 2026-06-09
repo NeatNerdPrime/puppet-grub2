6f1acf846ca6b2b11d991a8ea37e9ccd1b632c5b
Dennis Schreiber <dennis.schreiber@noris.de>
2026-06-09 11:08:29 +0200

    Enable use of "$password_username" and "$password_pbkdf2_hash" parameters.
    Even if the parameters "$password_username" and "$password_pbkdf2_hash" are set, they are not
    passed to the class "grub2::config" by "init.pp". This commit fixes this situation and transfers
    the values, if they are set, to the "grub2::config" class. The logic of the file then sets the
    values into the "$password_file" file.

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,8 @@
 # @param password Whether to manage password file.
 # @param password_file Path to password file.
 # @param password_template Template used for password file.
+# @param password_username GRUB superuser name.
+# @param password_pbkdf2_hash PBKDF2 hash for GRUB password.
 # @param default_entry GRUB default entry index/name.
 # @param timeout_style Timeout style.
 # @param timeout_style_disable Whether hidden timeout overrides timeout_style.
@@ -37,6 +39,8 @@ class grub2::config (
   Boolean $password,
   Stdlib::Absolutepath $password_file,
   String $password_template,
+  String $password_username,
+  String $password_pbkdf2_hash,
   String $default_entry,
   String $timeout_style,
   Boolean $timeout_style_disable,
